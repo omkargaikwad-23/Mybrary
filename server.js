472e5663ffb3c6588a627c437eec24438c366b63
Omkar Gaikwad <omkargaikwad9552@gmail.com>
2021-06-16 12:53:12 +0530

    set all files

---
+++ server.js
@@ -0,0 +1,28 @@
+//npm run devStart
+// if(process.env.NODE_ENV !== 'production') {
+//   require('dotenv').parse() //or use .load() instead
+// }
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')  //setting view engine
+app.set('views', __dirname+'/views') //from where views are coming
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+// const mongoose = require('mongoose')
+// mongoose.connect(process.env.DATABASE_URL, {
+//   useNewUrlParser: true})
+// const db = mongoose.connection
+// db.on('error', error => console.error(error))
+// db.once('open', () => console.log('Connected to Mongoose'))
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000)
+
