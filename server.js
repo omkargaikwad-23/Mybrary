7599f50f05be3402509401ed656c695f538bbf72
Omkar Gaikwad <omkargaikwad9552@gmail.com>
2021-06-17 09:18:22 +0530

    set up localhost done

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //npm run devStart
-// if(process.env.NODE_ENV !== 'production') {
-//   require('dotenv').parse() //or use .load() instead
-// }
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config() 
+}
 
 const express = require('express')
 const app = express()
@@ -15,12 +15,11 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
-// const mongoose = require('mongoose')
-// mongoose.connect(process.env.DATABASE_URL, {
-//   useNewUrlParser: true})
-// const db = mongoose.connection
-// db.on('error', error => console.error(error))
-// db.once('open', () => console.log('Connected to Mongoose'))
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 
