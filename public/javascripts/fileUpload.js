e6f0258de675e1279626839ab95a002eb9da6cde
Omkar Gaikwad <omkargaikwad9552@gmail.com>
2021-06-25 21:26:51 +0530

    styling to add,view book done

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,14 +1,28 @@
+const rootStyles = window.getComputedStyle(document.documentElement)
+
+if(rootStyles.getPropertyValue('--book-cover-width-large') != null && rootStyles.getPropertyValue('--book-cover-width-large') !== ''){
+  ready()
+}else{
+  document.getElementById('main-css').addEventListener('load', ready)
+}
+
 //using the plugins which we have included as CDN
-FilePond.registerPlugin(
-  FilePondPluginImagePreview,
-  FilePondPluginImageResize,
-  FilePondPluginFileEncode,
-)
+function ready() {
+  const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
+  const aspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio')) 
+  const coverHeight = parseFloat(coverWidth / aspectRatio);
+  FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginFileEncode,
+  )
+  
+  FilePond.setOptions({
+    stylePanelAspectRatio: 1 / aspectRatio,
+    imageResizeTargetWidth: coverWidth,
+    imageResizeTargetHeight: coverHeight
+  })
+  
+  FilePond.parse(document.body)
+}
 
-FilePond.setOptions({
-  stylePanelAspectRatio: 150 / 100,
-  imageResizeTargetWidth: 100,
-  imageResizeTargetHeight: 150
-})
-
-FilePond.parse(document.body)
